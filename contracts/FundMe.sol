68646ed08b1f32850eaeb0d9cf8bbdd660752c76
c0rdeiro <franciscordeiro99@gmail.com>
2025-04-15 16:27:49 +0100

    withdraw function and require owner (added constructor)

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,19 +6,37 @@ contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public minimunUsd = 5e18;
-    
+
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimunUsd, "Did not send enough ETH."); //1 ETH
         
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender]+= msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
 
     }
 
-    // function withdraw() public {}
+    function withdraw() public {
+        require(msg.sender == owner, "Must be the owner of the contract");
+
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex ++) {
+            
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+            funders = new address[](0);
+
+            (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+            require(callSuccess, "Withdraw failed.");
+        }
+    }
 
   
 }
